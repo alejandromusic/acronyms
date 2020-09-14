1bd5648cbee968cd0aec4edfdd63519aae861b77
alex <alex@music.com>
2020-09-14 07:18:38 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+interface Acron {
+  name: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'acronyms';
+
+  acronyms: Acron[] = [
+    {name: "LPG", description: "Lollipop Guild"},
+    {name: "SATCOM", description: "Satellite Communications"},
+    {name: "PTES", description: "Protected Tactical Enterprise Service"},
+    {name: "PTW", description: "Protected Tactical Waveform"},
+    {name: "PTAS", description: "Protected Tactical Anti-jam Satellite Communication"},
+  ]
+  displayedColumns: string[] = [
+    "name", "description"
+  ];
+
+
+  dataSource = new MatTableDataSource(this.acronyms);
+  showNew: boolean = false;
+
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log(filterValue.trim());
+    this.dataSource.filter = filterValue.trim();
+  }
+
+  model: Acron = {name: "ALEX", description: "Allied Lions Elegant Xylophones"};
+
+  save() {
+    
+    if (this.showNew) {
+
+      const m = {...this.model};
+      this.acronyms = [...this.acronyms, m];
+      this.dataSource.data = this.acronyms;
+    }
+    
+    this.showNew = !this.showNew;
+
+  }
+
+  update(row: Acron) {
+    this.model = row;
+    this.showNew = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
